f346e562be560992db4253fa2fd6845231427592
ailtonbsj <ailton.ifce@gmail.com>
2024-07-02 01:07:53 -0300

    using profiles of application.properties

--- vendas/src/main/java/io/github/ailtonbsj/MyConfigurationDev.java
+++ vendas/src/main/java/io/github/ailtonbsj/MyConfigurationDev.java
@@ -1,14 +1,22 @@
 package io.github.ailtonbsj;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("production")
-public class MyConfigurationProd {
+@Profile("development")
+public class MyConfigurationDev {
     @Bean(name = "applicationName")
     public String applicationName() {
-        return "Sistema de Vendas";
+        return "Sistema de Vendas DEV";
+    }
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+          System.out.print("RUNNING DEV CONF");
+        };
     }
 }
