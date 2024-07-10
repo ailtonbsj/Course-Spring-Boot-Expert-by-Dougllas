a6710e26e506b93384022d5c669eb30035a2de39
ailtonbsj <ailton.ifce@gmail.com>
2024-07-10 01:13:12 -0300

    Using user detail service

---
+++ 08-using-security/src/main/java/io/github/ailtonbsj/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package io.github.ailtonbsj.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(!username.equals("admin")){
+            throw new UsernameNotFoundException("User not found!");
+        }
+
+        return User.builder()
+                .username("admin")
+                .password(encoder.encode("123"))
+                .roles("USER", "ADMIN")
+                .build();
+    }
+}
